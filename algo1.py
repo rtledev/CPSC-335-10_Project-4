47789f48120d4097d61d8f79e4341653cd78c394
rtledev <149290741+rtledev@users.noreply.github.com>
2026-05-03 21:23:52 -0700

    Add author comments to algo1.py
    Add author information and email addresses to algo1.py

---
+++ algo1.py
@@ -0,0 +1,8 @@
+#Richard Le
+#Richard.le@csu.fullerton.edu
+#Marco Chavez
+#marco_chavez@csu.fullerton.edu
+#Arman Madatyan 
+#armanmadatyan@csu.fullerton.edu
+#Jeremy Mejia
+#fr.jeremy@csu.fullerton.edu
